5690da6117bfa18cab54fdafed492e9edd1b4cd8
khanhvn06 <46999108+khanhvn06@users.noreply.github.com>
2021-04-16 17:34:30 +0200

    Initial commit

---
+++ technicaltests-master-backend/backend/TestProgrammationConformit/TestProgrammationConformit/Infrastructures/ConformitContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestProgrammationConformit.Infrastructures
+{
+    public class ConformitContext : DbContext
+    {
+        public ConformitContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
